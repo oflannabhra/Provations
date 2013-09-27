e8d6625dd691231d4b4068f45df1cd4a0e560b5d
Ben Flannery <oflannabhra@gmail.com>
2013-09-27 16:01:12 -0400

    temp

---
+++ ProvationsApp/UIImage+iPad.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+UIImage_iPad.h
+//  ProvationsApp
+//
+//  Created by Ben Flannery on 3/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (UIImage_iPad)
+
+@end
---
+++ ProvationsApp/UIImage+iPad.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+UIImage_iPad.m
+//  ProvationsApp
+//
+//  Created by Ben Flannery on 3/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "UIImage+UIImage_iPad.h"
+
+@implementation UIImage (UIImage_iPad)
+
+@end
